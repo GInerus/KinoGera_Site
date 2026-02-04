47b5c5a4392003602b17cb318a0078e33beaebe7
Gera IRS <101325313+GInerus@users.noreply.github.com>
2026-02-05 00:12:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 async function loadPartial(id, file) {
-const response = await fetch(file);
-document.getElementById(id).innerHTML = await response.text();
+    const response = await fetch(file);
+    document.getElementById(id).innerHTML = await response.text();
 }
 
-
-loadPartial('header', '../partials/header.html');
-loadPartial('footer', '../partials/footer.html');
+loadPartial('header', './partials/header.html');
+loadPartial('footer', './partials/footer.html');
